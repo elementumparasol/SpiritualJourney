c342d71183373f968869c0bc93d2b3c2c740428d
enricashi <aishiqichun@163.com>
2018-03-30 01:38:36 +0800

    返回x的n次幂的另外一种算法

--- 明解C语言(入门篇)/第06章 函数/0605-值传递/0605-值传递/main.c
+++ 明解C语言(入门篇)/第06章 函数/0605-值传递/0605-值传递/main.c
@@ -26,6 +26,20 @@ double power(double x, int n) {
     return tmp;
 }
 
+/**
+ * 还是返回x的n次幂
+ */
+double anotherPower(double x, int n) {
+    
+    double tmp = 1.0;
+    
+    while (n-- > 0) {
+        tmp *= x;
+    }
+    
+    return tmp;
+}
+
 int main(void) {
     
     double a;
