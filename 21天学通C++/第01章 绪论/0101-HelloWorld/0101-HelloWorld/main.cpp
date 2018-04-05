e8ff9c8981826b3faaf19d93249108d577c6296b
enricashi <aishiqichun@163.com>
2018-04-05 10:27:07 +0800

    第一个C++程序

---
+++ 21天学通C++/第01章 绪论/0101-HelloWorld/0101-HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  0101-HelloWorld
+//
+//  Created by Enrica on 2018/4/5.
+//  Copyright © 2018年 Enrica. All rights reserved.
+//
+
+/**
+ * 第一个C++程序，照例输出Hello, World!
+ */
+
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
