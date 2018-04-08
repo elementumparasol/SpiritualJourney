83e6d06894f07d88a635a0ddcf6f5ec74a9ad796
enricashi <aishiqichun@163.com>
2018-04-08 15:02:41 +0800

    strlen函数和strcpy函数的使用

--- 21天学通C++/第09章 类和对象/0907-析构函数/0907-析构函数/main.cpp
+++ 21天学通C++/第09章 类和对象/0907-析构函数/0907-析构函数/main.cpp
@@ -24,10 +24,20 @@ public:
     // 定义构造函数
     MyString(const char *initString) {
         
+        // 判断输入的字符串是否为空
         if (initString != NULL) {
-            buffer = new char [strlen(initString - 1)];
-            strcpy(buffer, initString);
+            
+            // 使用strlen函数确定输入字符串的长度，然后再使用
+            // new为这个字符串分配内存空间，并且对指针buffer
+            // 进行初始化(没有进行初始化的指针，它是不能访问的)
+            buffer = new char [strlen(initString - 1)];  // 1、初始化指针(确定指针自身的内存地址)
+            
+            // 使用函数strcpy将输入的字符串复制到指针buffer指向
+            // 的内存中(也就是让指针buffer指向新输入的字符串)
+            strcpy(buffer, initString);  // 2、将指针buffer指向某个特定的值
         } else {
+            
+            // 如果输入的字符串为空，则将指针初始化为NULL
             buffer = NULL;
         }
     }
@@ -36,6 +46,7 @@ public:
     ~MyString() {
         cout << "MyString对象被释放掉了。" << endl;
         if (buffer != NULL) {
+            
             // 释放掉手动分配的内存
             delete [] buffer;
         }
