d9b4b2daeed0767c8716277389c6747ff3fbf26e
enricashi <aishiqichun@163.com>
2018-04-13 13:27:09 +0800

    Xcode 4.4之后的@property

---
+++ Objective-C基础查漏/01-面向对象/03-setter和getter的简便写法/03-setter和getter的简便写法/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  03-setter和getter的简便写法
+//
+//  Created by Enrica on 2018/4/13.
+//  Copyright © 2018年 Enrica. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
