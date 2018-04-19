eb06a9ca45c030eb1ba717ba5cee1648b893fb14
enricashi <aishiqichun@163.com>
2018-04-19 20:58:25 +0800

    指针的类型

---
+++ 深入理解C指针/第01章 认识指针/02-指针的长度和类型/02-指针的长度和类型/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  02-指针的长度和类型
+//
+//  Created by Enrica on 2018/4/18.
+//  Copyright © 2018年 Enrica. All rights reserved.
+//
+
+#include <stdio.h>
+
+/**
+ * 指针的预定义类型:
+ *  (1)、size_t: 用于安全的表示长度；
+ *  (2)、ptrdiff_t: 用于处理指针运算符；
+ *  (3)、intptr_t: 用于存储指针地址；
+ *  (4)、uintptr_t: 用于存储指针地址。
+ */
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
