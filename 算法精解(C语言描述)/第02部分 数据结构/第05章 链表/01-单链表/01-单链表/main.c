296f0a7d53e9660d74cc6148273dd57586721a10
enricashi <aishiqichun@163.com>
2018-04-28 00:27:25 +0800

    单链表初始化

---
+++ 算法精解(C语言描述)/第02部分 数据结构/第05章 链表/01-单链表/01-单链表/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  01-单链表
+//
+//  Created by Enrica on 2018/4/28.
+//  Copyright © 2018年 Enrica. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
