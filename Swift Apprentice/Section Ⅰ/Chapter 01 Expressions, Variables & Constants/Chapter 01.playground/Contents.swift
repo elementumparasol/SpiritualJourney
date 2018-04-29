7231b5be86dee917b1b30a4a31768cc72df21442
enricashi <aishiqichun@163.com>
2018-04-29 23:47:30 +0800

    表达式，变量和常量的补充

---
+++ Swift Apprentice/Section Ⅰ/Chapter 01 Expressions, Variables & Constants/Chapter 01.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// 位移操作
+1 << 3  // 8
+32 >> 2  // 8
+
+
+// 数学函数
+sin(45 * Double.pi / 180)  // 0.7071067811865475
+cos(135 * Double.pi / 180)  // -0.7071067811865475
+
+3.0.squareRoot()  // 1.732
+2.0.squareRoot()  // 1.414
+
+max(10, 20)  // 20
+min(-99, 3.6)  // -99
+
+// 自增和自减操作符
+var counter: Int = 0
+counter += 1  // 1
+counter -= 1  // 0
