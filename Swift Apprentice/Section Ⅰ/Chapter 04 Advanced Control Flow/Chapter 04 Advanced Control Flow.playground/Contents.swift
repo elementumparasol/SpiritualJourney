2b97126836ec7e491449cc507b03a94827b166fa
enricashi <aishiqichun@163.com>
2018-05-01 01:22:42 +0800

    for循环的基本使用

--- Swift Apprentice/Section Ⅰ/Chapter 04 Advanced Control Flow/Chapter 04 Advanced Control Flow.playground/Contents.swift
+++ Swift Apprentice/Section Ⅰ/Chapter 04 Advanced Control Flow/Chapter 04 Advanced Control Flow.playground/Contents.swift
@@ -13,3 +13,31 @@ let closedRange = 0...5  // 取值(0, 1, 2, 3, 4, 5)， 即[0, 5]
 
 // 半开半闭区间
 let halfOpenRange = 0..<5  // 取值(0, 1, 2, 3, 4)， 即[0, 5)
+
+
+
+/** 2、for循环 */
+
+let count = 10
+var sum = 0
+for i in 0..<count {
+    sum += i
+    print("sum = \(sum)")
+}
+
+
+sum = 1
+var lastSum = 0
+for _ in 0...count {
+    let tmp = sum
+    sum = sum + lastSum
+    lastSum = tmp
+    print("lastSum = \(lastSum)")
+}
+
+
+sum = 0
+for i in 0...count where i % 2 == 1 {  // 使用关键字where对i的取值进行过滤(i只取奇数)
+    sum = sum + i
+    print("sum = \(sum)")
+}
