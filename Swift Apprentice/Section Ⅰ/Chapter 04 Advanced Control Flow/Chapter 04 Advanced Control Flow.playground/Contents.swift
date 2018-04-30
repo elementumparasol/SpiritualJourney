fd868f55dc4c36db4b8137942bce7e82aba0dc9e
enricashi <aishiqichun@163.com>
2018-05-01 01:14:54 +0800

    区间运算符

---
+++ Swift Apprentice/Section Ⅰ/Chapter 04 Advanced Control Flow/Chapter 04 Advanced Control Flow.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+/** 1、范围运算符 */
+
+// 闭区间运算符
+let closedRange = 0...5  // 取值(0, 1, 2, 3, 4, 5)， 即[0, 5]
+
+// 半开半闭区间
+let halfOpenRange = 0..<5  // 取值(0, 1, 2, 3, 4)， 即[0, 5)
