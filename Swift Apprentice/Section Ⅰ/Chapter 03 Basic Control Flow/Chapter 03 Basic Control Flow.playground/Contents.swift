89dfae478809bdbba093ced4fde84e7d967dbec1
enricashi <aishiqichun@163.com>
2018-04-30 22:53:57 +0800

    Bool类型

---
+++ Swift Apprentice/Section Ⅰ/Chapter 03 Basic Control Flow/Chapter 03 Basic Control Flow.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+/** 1、Bool类型 */
+
+// 基本的Bool类型
+let yes = true  // Bool类型的结果只有一种，要么是true，要么就是false
+let no = false
+
+let doesOneEqualTwo = (1 == 2)
+print(doesOneEqualTwo)
+
+let doesOneNotEqualTwo = (1 != 2)
+print(doesOneNotEqualTwo)
+
+let alsoTure = !(1 == 2)
+print(alsoTure)
+
+let oneIsGreaterThanTwo = (1 > 2)
+print(oneIsGreaterThanTwo)
+
+let oneIsLessThanTwo = (1 < 2)
+print(oneIsLessThanTwo)
+
+let and = true && true
+let or = true || true
