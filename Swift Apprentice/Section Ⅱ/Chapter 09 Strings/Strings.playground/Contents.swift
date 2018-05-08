97daf76268f1a06fd22f5c953c9da60b68cc2088
enricashi <aishiqichun@163.com>
2018-05-08 23:13:29 +0800

    字符串的逆转类型ReversedCollection<String>

--- Swift Apprentice/Section Ⅱ/Chapter 09 Strings/Strings.playground/Contents.swift
+++ Swift Apprentice/Section Ⅱ/Chapter 09 Strings/Strings.playground/Contents.swift
@@ -31,3 +31,21 @@ let lastChar = enrica[lastIndex]  // 取出来的是字符a
 // 计算字符串的第4个索引
 let fourthIndex = enrica.index(enrica.startIndex, offsetBy: 3)
 let fourthChar = enrica[fourthIndex]  // 取出来的是字符i
+
+
+
+/** 2、字符串是双向集合 */
+let name = "James"
+let backwardsName = name.reversed()  // backwards是ReversedCollection<String>类型
+
+// 尽管backwards是ReversedCollection<String>类型，但是你仍然可以像使用String类型一样来使用它
+let nameLenght = enrica.count
+let firstIndexOfName = name.startIndex
+let firstCharOfName = name[firstIndexOfName]
+let lastIndexOfName = name.index(before: name.endIndex)
+let lastCharOfName = name[lastIndexOfName]
+let thirdIndexOfName = name.index(name.startIndex, offsetBy: 2)
+let thirdCharOfName = name[thirdIndexOfName]
+
+// 将ReversedCollection<String>类型将转成String类型
+let backwardsNameString = String(backwardsName)
