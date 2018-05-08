5d163ddf30f0d1ae2ef3c155ec5706c7510593aa
enricashi <aishiqichun@163.com>
2018-05-08 23:34:03 +0800

    Substring类型

--- Swift Apprentice/Section Ⅱ/Chapter 09 Strings/Strings.playground/Contents.swift
+++ Swift Apprentice/Section Ⅱ/Chapter 09 Strings/Strings.playground/Contents.swift
@@ -49,3 +49,27 @@ let thirdCharOfName = name[thirdIndexOfName]
 
 // 将ReversedCollection<String>类型将转成String类型
 let backwardsNameString = String(backwardsName)
+
+
+
+
+/** 3、SubString(字符串String的子类类型) */
+
+let fullName = "LeBron James"
+
+// 计算出fullName中的空字符所在的索引
+let spaceIndex = fullName.index(of: " ")!
+
+// 从fullName中截取出firstName
+let firstName = fullName[fullName.startIndex..<spaceIndex]  // 它是一个Substring类型，可以用fullName[..<spaceIndex]
+
+// 使用单边范围从fullName中截取出lastName
+//let afterSpaceIndex = fullName.index(spaceIndex, offsetBy: 1)  // 算出空字符后面一位字符的索引
+let afterSpaceIndex = fullName.index(after: spaceIndex)  // 也是算出空字符后面一位字符的索引
+let lastName = fullName[afterSpaceIndex...]
+
+// 使用子字符串类型(Substring)的好处是什么？就是当你从字符串类型(String)的变量
+// 中截取子字符串的时候，不用额外的开辟存储空间。当你想从Substring类型的子字符串
+// 中创建String类型的字符串时，才会开辟新的存储空间。Substring类型和String类型
+// 公用绝大部分属性或者方法，在使用Substring的过程中，如果不是强制要求使用String
+// 类型，你根本就不会意识到Substring和String有什么不同
