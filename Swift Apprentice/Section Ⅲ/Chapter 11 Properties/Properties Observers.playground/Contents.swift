04b316aba685a234649f3740319daa18c2d6eb80
enricashi <aishiqichun@163.com>
2018-05-13 09:14:04 +0800

    利用属性监听对结构体的属性进行限制

--- Swift Apprentice/Section Ⅲ/Chapter 11 Properties/Properties Observers.playground/Contents.swift
+++ Swift Apprentice/Section Ⅲ/Chapter 11 Properties/Properties Observers.playground/Contents.swift
@@ -30,3 +30,42 @@ struct Level {
 
 let myLevel = Level(id: 5, boss: "enrica", unlocked: true)
 let highestLevel = Level.highestLevel
+
+
+// 利用属性监听器对一个变量进行限制
+struct LightBulb {
+    
+    // 类型属性
+    static let maxCurrent = 40
+    
+    // 属性监听器
+    var current = 0 {
+        
+        // 监听属性current的改变
+        didSet {
+            if current > LightBulb.maxCurrent {
+                print("当前的值过大，还是回到之前的值")
+                current = oldValue
+            }
+        }
+    }
+}
+
+var light = LightBulb()
+
+// 设置结构体属性current的值
+light.current = 50
+var current = light.current  // 一旦current > LightBult.maxCurrent, 那么current = 0
+print("current = \(current)")
+
+// 重新设置结构体属性current的值
+light.current = 30
+current = light.current
+print("current = \(current)")
+
+/**
+ * current的值是有限制的，一旦current > LightBult.maxCurrent，
+ * 则current的值就设置失败；只有当current <= LightBult.maxCurrent
+ * 的时候，current的值才能设置成功
+ */
+
