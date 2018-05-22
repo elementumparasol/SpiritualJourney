8a04c9ace7afd23b8742e9e570d9b3e885ef2d28
enricashi <aishiqichun@163.com>
2018-05-22 22:57:20 +0800

    map函数的实现

--- Advanced Swift/第02章 内建集合类型/数组变形.playground/Contents.swift
+++ Advanced Swift/第02章 内建集合类型/数组变形.playground/Contents.swift
@@ -4,6 +4,8 @@ import UIKit
 
 var str = "Hello, playground"
 
+/** 1、map函数的应用示例 */
+
 // 数组变形(形变): 创建⼀个新数组，对已有数组中的元素进⾏循环依次取出其中元素，
 // 对取出的元素进⾏操 作，并把操作的结果加⼊到新数组的末尾。或者通俗的将，就是
 // 对数组中所有的元素按照某种标准做相同的操作
@@ -18,3 +20,27 @@ squared  // [0, 1, 1, 4, 9, 25, 64], 也就是相当于在原来数组fibs的基
 // Swift中有一个map函数可以很方便的指向上述操作
 let squares = fibs.map {$0 * $0}  // fibs.map {fib in fib * fib}
 squares  // [0, 1, 1, 4, 9, 25, 64]
+
+
+
+
+/** 2、自己动手实现map函数 */
+extension Array {
+    
+    // Element是数组中包含的元素类型的占位符，T是元素转换之后的类型的占位符
+    // map函数本⾝并不关⼼Element和T究竟是什么，它们可以是任意类型。T的具体
+    // 类型将由调⽤者传⼊给map的transform⽅法的返回值类型来决定
+    func map<T>(_ transform: (Element) -> T) -> [T] {
+        
+        var result: [T] = []
+        
+        result.reserveCapacity(count)
+        
+        for x in self {
+            
+            result.append(transform(x))
+        }
+        
+        return result
+    }
+}
