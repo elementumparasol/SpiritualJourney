324b3d74b300e2e939d9316cf486b6b8b2a3acc5
enricashi <aishiqichun@163.com>
2018-05-25 07:33:17 +0800

    Booleans

---
+++ Programming in Swift/Booleans.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let yes: Bool = true
+let no: Bool = false
+
+let doesOneEqualTwo = (1 == 2)
+let doesOneNotEqualTwo = (1 != 2)
+
+let isOneGreaterThanTwo = ( 1 >= 2)
+let isOneLessThanTwo = (1 <= 2)
