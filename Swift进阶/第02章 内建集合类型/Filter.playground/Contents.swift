ec7d6de4e3a155d6b2f02452d6cad24b847148b4
enricashi <aishiqichun@163.com>
2018-05-26 21:19:25 +0800

    map和filter的结合使用

--- Swift进阶/第02章 内建集合类型/Filter.playground/Contents.swift
+++ Swift进阶/第02章 内建集合类型/Filter.playground/Contents.swift
@@ -4,6 +4,16 @@ import UIKit
 
 var str = "Hello, playground"
 
+
+/** 1、filter函数的基本使用 */
+
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let result = nums.filter{ $0 % 2 == 0 }  // { num in num % 2 == 0 } 这里的num使用前面的$0取代了
 result
+
+
+
+/** 2、map函数和filter函数的结合使用 */
+
+let mixedResult = (1..<10).map { $0 * $0 }.filter { $0 % 2 == 0 }
+mixedResult
