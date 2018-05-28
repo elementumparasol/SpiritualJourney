694027b0887ff7759c18506b68bdabea67b1fd20
enricashi <aishiqichun@163.com>
2018-05-29 06:57:24 +0800

    使用传统方式执行累加操作

---
+++ Swift进阶/第02章 内建集合类型/Reduce.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/** 1、使用传统方式进行累加计算 */
+
+let fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+var total = 0
+for num in fibs {
+    total = total + num
+}
+total
