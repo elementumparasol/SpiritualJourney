7896d799d6e15091d326f67b6d53d57c4833c645
enricashi <aishiqichun@163.com>
2018-06-02 13:11:21 +0800

    单边范围

--- Swift进阶/第02章 内建集合类型/Range.playground/Contents.swift
+++ Swift进阶/第02章 内建集合类型/Range.playground/Contents.swift
@@ -33,3 +33,26 @@ for i in 0..<10 {
 // 很相似，只不过它还需要⼀个附加约束：它的元素类型需要遵守Strideable协议(以整数为步⻓)
 // Swift将这类功能更强的范围叫做可数范围，这是因为只有这 类范围可以被迭代
 // 可数范围的边界可以是整数或者指针类型，但不能是浮点数类型，这是由于Stride类型中有⼀个整数的约束
+
+
+
+/** 3、部分范围 */
+
+// 部分范围 (partial range) 指的是将 ... 或 ..< 作为前置或者后置运算符来使⽤时所构造的范围
+// 这类范围由于缺少⼀侧的边界，因此被称为部分范围。部分范围也称之为单边范围
+
+// PartialRangeFrom不能被迭代，这是因为它的Bound不满⾜Strideable
+let fromA: PartialRangeFrom<Character> = Character("a")...
+
+// PartialRangeThrough则是因为没有下界⽽⽆法开始迭代
+let throughtZ: PartialRangeThrough<Character> = ...Character("z")
+
+
+// PartialRangeUpTo同样也是因为没有下界⽽⽆法开始迭代
+let upToTen: PartialRangeUpTo<Int> = ..<10  // 注意，这里<和数字10之间是没有空格的
+
+// 其中能够计数的只有CountablePartialRangeFrom这⼀种类型，四种部分范围类型中，只有它能被进⾏迭代
+let fromFive: CountablePartialRangeFrom<Int> = 5...
+
+// 迭代操作会从lowerBound开始，不断重复地调⽤advanced(by: 1)。如果你在⼀个for循环中使⽤这种范围，
+// 你必须牢记要为循环添加⼀个break的退出条件，否则循环将⽆限进⾏下去(或者当计数溢出的时候发⽣崩溃)
