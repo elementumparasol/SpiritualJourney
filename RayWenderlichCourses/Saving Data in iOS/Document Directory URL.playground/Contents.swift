7e190dbbf28d4fbf143633eedec1e259adadfdb8
enricashi <aishiqichun@163.com>
2018-06-07 22:33:57 +0800

    path

--- RayWenderlichCourses/Saving Data in iOS/Document Directory URL.playground/Contents.swift
+++ RayWenderlichCourses/Saving Data in iOS/Document Directory URL.playground/Contents.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+/*
 try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
 
 FileManager.documentDirectorURL
+*/
+
+
+//FileManager.documentDirectoryURL.path
+FileManager.documentDirectoryURL
+
+let mysteryDataURL = URL(fileURLWithPath: "mystery", relativeTo: FileManager.documentDirectoryURL)
+mysteryDataURL
+
+let stringURL = FileManager.documentDirectoryURL.appendingPathComponent("string").appendingPathExtension("txt")
+stringURL
--- RayWenderlichCourses/Saving Data in iOS/Document Directory URL.playground/Sources/FileManager.swift
+++ RayWenderlichCourses/Saving Data in iOS/Document Directory URL.playground/Sources/FileManager.swift
@@ -1,7 +1,22 @@
 import Foundation
 
+/*
 public extension FileManager {
     static var documentDirectorURL: URL {
         return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    }
+}
+*/
+
+
+public extension FileManager {
+    
+    static var documentDirectoryURL: URL {
+        return try! FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false
+        )
     }
 }
