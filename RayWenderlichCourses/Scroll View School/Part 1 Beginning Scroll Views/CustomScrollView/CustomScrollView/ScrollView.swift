22771ac44c448749eb89d752740877f1d671c4ec
enricashi <aishiqichun@163.com>
2018-06-17 23:41:00 +0800

    自定义scrollView

---
+++ RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/CustomScrollView/CustomScrollView/ScrollView.swift
@@ -0,0 +1,39 @@
+//
+//  ScrollView.swift
+//  CustomScrollView
+//
+//  Created by Enrica on 2018/6/17.
+//  Copyright © 2018年 Enrica. All rights reserved.
+//
+
+import UIKit
+
+class ScrollView: UIView {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // 创建pan手势
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panView(with:)))
+        
+        // 添加pan手势
+        addGestureRecognizer(panGesture)
+    }
+    
+    
+    /// 手势执行的方法
+    ///
+    /// - Parameter gestureRecognizer: 传入手势
+    @objc func panView(with gestureRecognizer: UIPanGestureRecognizer) {
+        
+        // 获取手势所在的坐标系
+        let translation = gestureRecognizer.translation(in: self)
+        
+        // 更改坐标
+        self.bounds.origin.y = self.bounds.origin.y - translation.y
+        
+        // 重新设置手势的坐标
+        gestureRecognizer.setTranslation(CGPoint.zero, in: self)
+    }
+
+}
