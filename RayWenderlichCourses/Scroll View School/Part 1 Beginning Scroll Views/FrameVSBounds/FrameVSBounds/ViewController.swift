d00f113c5017a69072e38125bc6b9bdaad9623d7
enricashi <aishiqichun@163.com>
2018-06-18 16:15:20 +0800

    通过slider来直观的查看frame和bounds的变化

--- RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/FrameVSBounds/FrameVSBounds/ViewController.swift
+++ RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/FrameVSBounds/FrameVSBounds/ViewController.swift
@@ -44,9 +44,64 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 获取frameXSlider和boundsXSlider的初始值
+        frameXSlider.value = Float(containerView.frame.origin.x)
+        boundsXSlider.value = Float(containerView.bounds.origin.x)
+        
+        // 获取FrameYSlider和boundsXSlider的初始值
+        FrameYSlider.value = Float(containerView.frame.origin.y)
+        boundsXSlider.value = Float(containerView.bounds.origin.y)
     }
 
-
+    /// frame的origin.x的值的改变
+    @IBAction func frameXSliderChanged(_ sender: Any) {
+        
+        // 将slider的值设置为containerView的frame的origin.x的值
+        containerView.frame.origin.x = CGFloat(frameXSlider.value)
+        
+        // 将修改的值更新到对应的label上去
+        updateValues()
+    }
+    
+    /// bounds的origin.x的值的改变
+    @IBAction func boundsXSliderChanged(_ sender: Any) {
+        
+        // 将slider的值设置为containerView的bounds的origin.x的值
+        containerView.bounds.origin.x = CGFloat(boundsXSlider.value)
+        
+        // 将修改的值更新到对应的label上去
+        updateValues()
+    }
+    
+    /// frame的origin.y的值的改变
+    @IBAction func frameYSliderChanged(_ sender: Any) {
+        
+        // 将slider的值设置为containerView的frame的origin.y的值
+        containerView.frame.origin.y = CGFloat(FrameYSlider.value)
+        
+        // 将修改的值更新到对应的label上去
+        updateValues()
+    }
+    
+    /// bounds的origin.y的值的改变
+    @IBAction func boundsYSliderChanged(_ sender: Any) {
+        
+        // 将slider的值设置为containerView的bounds的origin.y的值
+        containerView.bounds.origin.y = CGFloat(boundsYSlider.value)
+        
+        // 将修改的值更新到对应的label上去
+        updateValues()
+    }
+    
+    /// 实时更改label上面的文本
+    fileprivate func updateValues() {
+        
+        // 设置label上面的文字
+        frameXLabel.text = "Frame x origin = \(Int(containerView.frame.origin.x))"
+        boundsXLabel.text = "Bounds x origin = \(Int(containerView.bounds.origin.x))"
+        FrameYLabel.text = "Frame y origin = \(Int(containerView.frame.origin.y))"
+        boundsYLabel.text = "Bounds y origin = \(Int(containerView.bounds.origin.y))"
+    }
 }
 
