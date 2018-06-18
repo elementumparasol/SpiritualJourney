5de874a105f87dafabe20576f4318df6fc43c451
enricashi <aishiqichun@163.com>
2018-06-18 16:50:27 +0800

    在scrollView中滚动查看超大图片

--- RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/ImageScroller/ImageScroller/ViewController.swift
+++ RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/ImageScroller/ImageScroller/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - 控件属性
+    
+    /// scrollView控件
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 去掉编译器自动调整的安全区域(如果不设置这个属性，则顶部和底部都会有一个空白)
+        scrollView.contentInsetAdjustmentBehavior = .never
+        
+        // 去掉滚动到边缘之后的弹簧效果
+        scrollView.bounces = false
     }
 
-
+    /**
+     * scrollView的contentSize已经在storyboard中用KVC的方式设置过了
+     * 在scrollView中，如果不设置contentSize，那么它就无法实现滚动
+     */
 }
 
