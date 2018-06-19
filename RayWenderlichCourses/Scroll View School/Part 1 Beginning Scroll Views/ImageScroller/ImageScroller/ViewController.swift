7ef844c7f45d2f74d1bf5dba07dcaac6d2657a4f
enricashi <aishiqichun@163.com>
2018-06-20 00:56:17 +0800

    让大图自适应屏幕大小

--- RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/ImageScroller/ImageScroller/ViewController.swift
+++ RayWenderlichCourses/Scroll View School/Part 1 Beginning Scroll Views/ImageScroller/ImageScroller/ViewController.swift
@@ -23,43 +23,41 @@ class ViewController: UIViewController {
         
         // 获取imageView中图片的size
         guard let imageSize = imageView.image?.size else { return }
-        
+
         // 设置scrollView的contentSize
-        scrollView.contentSize = imageSize
-        
+//        scrollView.contentSize = imageSize  // 如果需要对scrollView中的内容进行滚动，则需要设置它的contentSize
+        imageView.frame.size = imageSize
+
         if #available(iOS 11.0, *) {
-            
+
             // 去掉编译器自动调整的安全区域(如果不设置这个属性，则顶部和底部都会有一个空白)
             scrollView.contentInsetAdjustmentBehavior = .never
         }
-        
+
         // 去掉滚动到边缘之后的弹簧效果
         scrollView.bounces = false
-        
-        
+
+
         // 设置代理
         scrollView.delegate = self
-        
-        
+
+
         setZoomForSize(scrollView.bounds.size)
     }
     
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        
-        print("-------------")
-        
+
         setZoomForSize(scrollView.bounds.size)
     }
 
     func setZoomForSize(_ scrollViewSize: CGSize) {
-        
+
         let imageSize = imageView.bounds.size
         let widthScale = scrollViewSize.width / imageSize.width
         let heightScale = scrollViewSize.height / imageSize.height
         let minScale = min(widthScale, heightScale)
-        
         scrollView.minimumZoomScale = minScale
         scrollView.maximumZoomScale = 3.0
         scrollView.zoomScale = minScale
@@ -68,9 +66,9 @@ class ViewController: UIViewController {
 
 // MARK: - UIScrollViewDelegate
 extension ViewController: UIScrollViewDelegate {
-    
-    //
-    public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+
+    // 缩放视图
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
 }
