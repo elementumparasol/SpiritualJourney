1b6bd959cf9f47bdacb6992cb4f75df1c3ccb491
enricashi <aishiqichun@163.com>
2018-06-27 22:29:49 +0800

    枚举的原始值

--- Swift基础查漏/第03部分 枚举、结构体和类/枚举.playground/Contents.swift
+++ Swift基础查漏/第03部分 枚举、结构体和类/枚举.playground/Contents.swift
@@ -13,4 +13,43 @@ enum TextAlignment {
 }
 
 var alignment: TextAlignment = TextAlignment.left
-alignment = .center  // 修改
+alignment = .center
+
+
+/** 2、原始值枚举 */
+
+// 使用默认原始值
+enum Weekday: Int {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
+
+print("monday的原始值是: \(Weekday.monday.rawValue)")
+print("tuesday的原始值是: \(Weekday.tuesday.rawValue)")
+print("wednesday的原始值是: \(Weekday.wednesday.rawValue)")
+print("thursday的原始值是: \(Weekday.thursday.rawValue)")
+print("friday的原始值是: \(Weekday.friday.rawValue)")
+
+// 使用指定原始值
+enum Weekend: Int {
+    case saturday = 30
+    case sunday = 50
+}
+
+print("saturday的原始值是: \(Weekend.saturday.rawValue)")
+print("sunday的原始值是: \(Weekend.sunday.rawValue)")
+
+// 其实，在Swift中，原始值除了支持整型之外，它还支持其它所有类型
+enum ProgrammingLanguage: String {
+    case swift = "Swift"
+    case java = "Java"
+    case python = "Python"
+    case ruby = "Ruby"
+    case cpp = "C++"
+}
+
+let myFavoriteLanguage = ProgrammingLanguage.swift
+print("我最喜欢的编程语言是: \(myFavoriteLanguage.rawValue)")
