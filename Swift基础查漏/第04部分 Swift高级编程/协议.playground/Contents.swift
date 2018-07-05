78f23e73cc72f3268bd0371388dbd52a9335a208
enricashi <aishiqichun@163.com>
2018-07-06 01:08:43 +0800

    协议的实例属性

--- Swift基础查漏/第04部分 Swift高级编程/协议.playground/Contents.swift
+++ Swift基础查漏/第04部分 Swift高级编程/协议.playground/Contents.swift
@@ -33,3 +33,45 @@ let dog = Dog()
 dog.walking()
 
 
+/** 2、协议中的实例属性 */
+
+protocol Person {
+    
+    var name: String { get set }
+    var age: Int { get set }
+    var height: Double { get set }
+}
+
+class Student: Person {
+    
+    // 遵守Person协议所需要实现的属性
+    var name: String
+    var age: Int
+    var height: Double
+    
+    // 自己的存储属性
+    var num: Int
+    
+    init(name: String, age: Int, height: Double, num: Int) {
+        
+        self.name = name
+        self.age = age
+        self.height = height
+        self.num = num
+    }
+}
+
+struct Teacher: Person {
+    
+    var name: String = ""
+    var age: Int = 0
+    var height: Double = 0
+    
+    var subject: String = "Math"
+}
+
+let s = Student(name: "James", age: 19, height: 1.73, num: 20180101)
+let t = Teacher(name: "Tom", age: 36, height: 1.81, subject: "biology")
+
+// 注意，协议作为类型使用的时候，基本上和其它类型没有太大区别，它不仅可以使用as、as?和as!
+// 进行对象类型转换，还可以使用is操作符类判断某个类型是否有遵守某个协议
