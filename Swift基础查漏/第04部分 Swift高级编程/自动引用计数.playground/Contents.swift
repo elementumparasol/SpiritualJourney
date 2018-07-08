24526e8588aed141b12c6cb111003b31ccc0dc61
enricashi <aishiqichun@163.com>
2018-07-08 20:56:32 +0800

    无主引用

--- Swift基础查漏/第04部分 Swift高级编程/自动引用计数.playground/Contents.swift
+++ Swift基础查漏/第04部分 Swift高级编程/自动引用计数.playground/Contents.swift
@@ -60,3 +60,56 @@ apt?.tenant = p  // 公寓的主人是James
 
 p = nil
 apt = nil
+
+
+/** 2、无主引用 */
+
+// 和弱引用类似，无主引用不会牢牢保持住引用的实例。和弱引用不同的是，无主引用在其他实例
+// 有相同或者更长的生命周期时使用。你可以在声明属性或者变量时，在前面加上关键字unowned
+// 表示这是一个无主引用。不过，需要注意的是，使用无主引用，你必须确保引用始终指向一个未
+// 销毁的实例。如果你试图在实例被销毁后，访问该实例的无主引用，会触发运行时错误
+
+class Customer {
+    
+    let name: String
+    
+    // 强引用
+    var card: CreditCard?
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    deinit {
+        print("\(name)被销毁了")
+    }
+}
+
+class CreditCard {
+    
+    let number: UInt64
+    
+    // 无主引用
+    unowned let customer: Customer
+    
+    init(number: UInt64, customer: Customer) {
+        self.number = number
+        self.customer = customer
+    }
+    
+    deinit {
+        print("Card #\(number)被销毁了")
+    }
+}
+
+var john: Customer?
+john = Customer(name: "John Appleseed")
+john!.card = CreditCard(number: 1234_5678_9012_3456, customer: john!)
+john = nil
+
+// 上面弱引用和无主引用的例子涵盖了两种常用的需要打破循环强引用的场景。
+// Person和Apartment的例子展示了两个属性的值都允许为nil，并会潜在的产生循环强引用。
+// 这种场景最适合用弱引用来解决。Customer和CreditCard的例子展示了一个属性的值允许
+// 为nil，而另一个属性的值不允许为nil，这也可能会产生循环强引用。这种场景最适合通过无
+// 主引用来解决。
+
