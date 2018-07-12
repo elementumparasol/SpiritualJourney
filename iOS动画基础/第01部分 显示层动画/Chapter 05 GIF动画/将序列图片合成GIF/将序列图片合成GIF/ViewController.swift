e37b796810f5fa13aff52a1b7d933048c1e92bbc
enricashi <aishiqichun@163.com>
2018-07-12 21:01:37 +0800

    创建目标文件的存放目录和全路径

--- iOS动画基础/第01部分 显示层动画/Chapter 05 GIF动画/将序列图片合成GIF/将序列图片合成GIF/ViewController.swift
+++ iOS动画基础/第01部分 显示层动画/Chapter 05 GIF动画/将序列图片合成GIF/将序列图片合成GIF/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import ImageIO
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
@@ -29,8 +31,49 @@ class ViewController: UIViewController {
             // 将图片存入数组中
             images.append(image)
         }
+        
+        
+        // MARK: - 在Documents文件夹下建立存储GIF图片的目录
+        
+        // 获取Documents在沙盒中的全路径
+        let documentsString = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        
+        // 在Documents目录下新建一个文件夹，用于存储合成之后的GIF图片
+        do {
+            try FileManager.default.createDirectory(atPath: "\(documentsString)/GIF/FunnyKanna.gif", withIntermediateDirectories: true, attributes: nil)
+        } catch let error {
+            debugPrint(error)
+        }
+        
+        // 拼接目标文件的全路径
+        let gifPath = documentsString + "/GIF/FunnyKanna.gif"
+        
+        print(documentsString)
+        
+        // 将目标文件FunnyKanna.gif的String路径转换为URL路径
+        guard let url = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, gifPath as CFString, .cfurlposixPathStyle, false) else { return }
+        
+        // 创建图片的目标对象
+        // - url: 需要写入的目标URL路径(The URL to write to. If the URL already exists, the data at this location is overwritten)
+        // - type: 图片类型(The UTI (uniform type identifier) of the resulting image file)
+        // - count: 图片的帧数(The number of images (not including thumbnail images) that the image file will contain)
+        // - options: 图片的属性(Reserved for future use. Pass NULL)
+        let imageDestination = CGImageDestinationCreateWithURL(url, kUTTypeGIF, images.count, nil)
+        
+        
+        
+        
     }
 
 
 }
 
+/*
+url - The URL to write to. If the URL already exists, the data at this location is overwritten.
+
+type - The UTI (uniform type identifier) of the resulting image file. See Uniform Type Identifiers Overview for a list of system-declared and third-party UTIs.
+
+count - The number of images (not including thumbnail images) that the image file will contain.
+
+options - Reserved for future use. Pass NULL.
+*/
