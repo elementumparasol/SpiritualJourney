f07546e7f0c08f8c6ff58a61108b2210058f54bd
enrica <aishiqichun@163.com>
2018-07-14 08:49:53 +0800

    淡入淡出效果

--- iOS动画基础/第02部分 内容层动画/Chapter 07 CAKeyframeAnimation和CAAinmationGroup动画/淡出动画/淡出动画/ViewController.swift
+++ iOS动画基础/第02部分 内容层动画/Chapter 07 CAKeyframeAnimation和CAAinmationGroup动画/淡出动画/淡出动画/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - 懒加载属性
+    
+    /// 创建blueView
+    private lazy var blueView: UIView = {
+        
+        let view = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 180, height: 180)))
+        view.backgroundColor = .blue
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 设置blueView的位置
+        blueView.center = view.center
+        
+        // 将blueView添加到当前控制器的view上
+        view.addSubview(blueView)
+        
+        // 播放动画
+        displayAnimation()
     }
 
-
+    /// 播放动画效果
+    private func displayAnimation() {
+        
+        // 创建CAKeyframeAnimation对象
+        let animation = CAKeyframeAnimation()
+        
+        // 设置动画执行时长
+        animation.duration = 5
+        
+        // 设置animation的keyPath(动画执行样式)
+        animation.keyPath = "opacity"
+        
+        // 创建动画的值values
+        let valuesArray = [0.95, 0.85, 0.75, 0.65, 0.55, 0.45, 0.35, 0.25, 0.15, 0.05]
+        
+        // 设置animation的values
+        animation.values = valuesArray
+        
+        // 设置fillMode的值为forwards, 即表示保留动画结束的最后状态
+        animation.fillMode = .forwards
+        
+        // 设置动画执行完成之后是否从layer上面移除
+        animation.isRemovedOnCompletion = false
+        
+        // 将动画添加到blueView的layer上
+        blueView.layer.add(animation, forKey: nil)
+    }
 }
 
