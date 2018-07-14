f981bfe3a04da7a0bfe24d1ac1cc37f373d45b43
enrica <aishiqichun@163.com>
2018-07-14 10:54:16 +0800

    创建登录按钮

---
+++ iOS动画基础/第02部分 内容层动画/Chapter 08 登录按钮动画/水波动画/水波动画/AnimationButtom.swift
@@ -0,0 +1,21 @@
+//
+//  AnimationButtom.swift
+//  水波动画
+//
+//  Created by Enrica on 2018/7/14.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import UIKit
+
+class AnimationButtom: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- iOS动画基础/第02部分 内容层动画/Chapter 08 登录按钮动画/水波动画/水波动画/ViewController.swift
+++ iOS动画基础/第02部分 内容层动画/Chapter 08 登录按钮动画/水波动画/水波动画/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - 懒加载属性
+    
+    /// 登录按钮
+    private lazy var loginButton: AnimationButtom = {
+        
+        // 创建按钮对象
+        let button = AnimationButtom(frame: CGRect(origin: .zero, size: CGSize(width: UIScreen.main.bounds.width - 2 * 20, height: 44)))
+        
+        // 设置按钮的背景颜色
+        button.backgroundColor = .red
+        
+        // 设置按钮的圆角
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 5
+        
+        // 设置按钮普通状态下的文字
+        button.setTitle("登录", for: .normal)
+        
+        // 设置按钮高亮状态下的文字
+        button.setTitle("登录中", for: .highlighted)
+        
+        // 设置按钮高亮状态下的文字颜色
+        button.setTitleColor(.lightGray, for: .highlighted)
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 设置按钮的位置
+        loginButton.center = view.center
+        
+        // 将按钮添加到当前控制器的view上
+        view.addSubview(loginButton)
     }
 
 
