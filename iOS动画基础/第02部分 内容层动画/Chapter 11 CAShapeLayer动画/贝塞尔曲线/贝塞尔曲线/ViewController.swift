8b16143bb340c739c5ca2cb1eaf9000db03498f0
enrica <aishiqichun@163.com>
2018-07-16 02:25:54 +0800

    添加drawView到控制器的view上

---
+++ iOS动画基础/第02部分 内容层动画/Chapter 11 CAShapeLayer动画/贝塞尔曲线/贝塞尔曲线/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  贝塞尔曲线
+//
+//  Created by Enrica on 2018/7/16.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - 懒加载属性
+    
+    /// drawView
+    private lazy var drawView: UIView = {
+        
+        let screenWidth = UIScreen.main.bounds.size.width
+        let screenHeight = UIScreen.main.bounds.size.height
+        
+        let view = UIView(frame: CGRect(origin: .zero, size: CGSize(width: screenWidth - 40, height: screenHeight - 40)))
+        view.backgroundColor = .lightGray
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 确定drawView控件的位置
+        drawView.center = view.center
+        
+        // 将drawView添加到当前控制器的view上
+        view.addSubview(drawView)
+    }
+}
+
