e6271305741904d7c0c28a1f5f739e63a57733b0
enrica <aishiqichun@163.com>
2018-07-22 19:07:18 +0800

    在C++文件中兼容C代码

--- 算法笔记(C:C++描述)/Chapter 02 C:C++基础/2.1-基本数据类型/2.1-基本数据类型/main.cpp
+++ 算法笔记(C:C++描述)/Chapter 02 C:C++基础/2.1-基本数据类型/2.1-基本数据类型/main.cpp
@@ -9,6 +9,13 @@
 #include <iostream>
 #include <cstdio>
 
+/**
+ * C++是C的超级，因此，可以直接在C++文件中写C代码。只不过，在C++文件中导入
+ * C的头文件时，不再是导入.h文件，而是导入的时候去掉.h，并且在文件名开头加上
+ * 小写字母c，eg: 在C语言中导入标准输入输出文件#include <stdio.h>，在C++
+ * 中则变成了#include <cstdio>，类似的有#include <cmath>、#include <cstring>等等
+ */
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
