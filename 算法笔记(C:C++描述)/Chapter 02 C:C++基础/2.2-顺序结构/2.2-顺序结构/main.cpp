b63e52ddd3184a7d6b11f68d334f93488d0eceb4
enrica <aishiqichun@163.com>
2018-07-22 21:43:16 +0800

    绝对值函数fabs(double x)

---
+++ 算法笔记(C:C++描述)/Chapter 02 C:C++基础/2.2-顺序结构/2.2-顺序结构/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  2.2-顺序结构
+//
+//  Created by Enrica on 2018/7/22.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main(int argc, const char * argv[]) {
+    
+    /** 1、fabs(double x)函数 */
+    
+    double db = -12.56;
+    printf("|db| = %.2f\n", fabs(db));
+    
+    return 0;
+}
