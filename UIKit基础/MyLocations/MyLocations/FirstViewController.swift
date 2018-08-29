fafda03e9ffcb1ad5d355762c5bcb42244cd4234
enrica <aishiqichun@163.com>
2018-08-29 22:58:10 +0800

    init MyLocations

---
+++ UIKit基础/MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Enrica on 2018/8/29.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UIKit基础/MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Enrica on 2018/8/29.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
