0cbff59c6cbd4611bc00b4dc53b33e7491f2af92
enrica <aishiqichun@163.com>
2018-08-30 14:46:43 +0800

    约束子控件的位置

--- UIKit基础/MyLocations/MyLocations/CurrentLocationViewController.swift
+++ UIKit基础/MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -36,6 +36,9 @@ class CurrentLocationViewController: UIViewController {
     
     /// locationManager
     var locationManager =  CLLocationManager()
+    
+    /// 用于存储当前的位置信息
+    var location: CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,12 +57,13 @@ class CurrentLocationViewController: UIViewController {
             return
         }
         
-        // 如果获取地理位置被禁止了，则弹出alert提示
+        // 如果获取地理位置新的的授权被禁止或者限制了，就会弹出alert进行提示
         if authStatus == .denied || authStatus == .restricted {
             showLocationServicesDeniedAlert()
             return
         }
         
+        // 设置当前控制器为locationManager的代理
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.startUpdatingLocation()
@@ -71,15 +75,42 @@ class CurrentLocationViewController: UIViewController {
     /// 如果获取地理位置被禁止，则弹出提示
     func showLocationServicesDeniedAlert() {
         
+        // 创建alertController
         let alert = UIAlertController(title: "Location Services Disabled",
                                       message: "Please enable location services for this app in Settings.",
                                       preferredStyle: .alert)
         
+        // 创建action
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         
+        // 将action添加到alert控制器中
         alert.addAction(okAction)
         
+        // 弹出alert控制器
         present(alert, animated: true, completion: nil)
+    }
+    
+    /// 更新文本控件上面的内容
+    func updateLabels() {
+        
+        if let location = location {
+            
+            // 更新纬度信息
+            latitudelabel.text = String(format: "%.8f", location.coordinate.latitude)
+            
+            // 更新经度信息
+            longitudelabel.text = String(format: "%.8f", location.coordinate.longitude)
+            
+            //
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudelabel.text = ""
+            longitudelabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start."
+        }
     }
 }
 
@@ -93,7 +124,15 @@ extension CurrentLocationViewController: CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let newLocation = locations.last!
+        
+        // 为什么要用最后一个？因为最后一个位置消息是最新的
+        let newLocation = locations.last!  // 获取位置信息
         print("didUpdateLocations: \(newLocation)")
+        
+        // 将最新的位置信息保存到location中
+        location = newLocation
+        
+        // 更新label上面的文本内容
+        updateLabels()
     }
 }
