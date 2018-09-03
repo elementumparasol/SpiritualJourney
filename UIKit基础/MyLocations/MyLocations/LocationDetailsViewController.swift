78cae574b01543aadb315f4f370607436e969ce7
enrica <aishiqichun@163.com>
2018-09-03 23:00:49 +0800

    修改行高

--- UIKit基础/MyLocations/MyLocations/LocationDetailsViewController.swift
+++ UIKit基础/MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -156,3 +156,28 @@ class LocationDetailsViewController: UITableViewController {
     
     
 }
+
+
+
+// MARK: - UITableViewDelegate
+extension LocationDetailsViewController {
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize(width: view.bounds.size.width - 120, height: 1000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 16
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
+    
+    
+    
+    
+    
+}
