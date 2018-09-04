fb21128440631f3bae06b155b556f278ed88058f
enrica <aishiqichun@163.com>
2018-09-04 23:52:49 +0800

    绘制一个HUD

--- UIKit基础/MyLocations/MyLocations/HudView.swift
+++ UIKit基础/MyLocations/MyLocations/HudView.swift
@@ -47,5 +47,11 @@ class HudView: UIView {
         
         roundedRect.fill()
         
+        
+        if let image = UIImage(named: "Checkmark") {
+            let imagePoint = CGPoint(x: center.x - round(image.size.width / 2), y: center.y - round(image.size.height / 2) - boxHeight / 8)
+            
+            image.draw(at: imagePoint)
+        }
     }
 }
