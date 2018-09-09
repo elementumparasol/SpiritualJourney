0deaff84c4ae1b73aad44ea0e3b4680c9bdc76ff
enrica <aishiqichun@163.com>
2018-09-09 15:30:36 +0800

    修改DataModel以及与其相关联的描述性类文件

--- UIKit基础/MyLocations/MyLocations/Location+CoreDataClass.swift
+++ UIKit基础/MyLocations/MyLocations/Location+CoreDataClass.swift
@@ -30,4 +30,47 @@ public class Location: NSManagedObject, MKAnnotation {
     public var subtitle: String? {
         return category
     }
+    
+    /// 用于判断Location对象是有拥有图片(photo)
+    var hasPhoto: Bool {
+        return photoID != nil
+    }
+    
+    /// PhotoURL用于计算图片的全路径
+    var photoURL: URL {
+        
+        // 使用断言来检查photoID是否有值
+        // 如果没有值，程序会崩溃并给出信息
+        assert(photoID != nil, "没有设置图片ID")
+        
+        // 给图片命名
+        let filename = "Photo-\(photoID!.intValue).jpg"
+        
+        // 拼接图片在Documents文件夹中的全路径，并且将其返回
+        return applicationDocumentsDirectory.appendingPathComponent(filename)
+    }
+    
+    /// 加载图片
+    var photoImage: UIImage? {
+        
+        // 根据路径加载图片，并且将其返回
+        return UIImage(contentsOfFile: photoURL.path)
+    }
+    
+    /// 返回图片的ID
+    class func nextPhotoID() -> Int {
+        
+        let userDefaults = UserDefaults.standard
+        
+        // 用指定的键返回一个整数值，然后在此基础上加1，将其作为currentID
+        let currentID = userDefaults.integer(forKey: "PhotoID") + 1
+        
+        // 每一个NSManagedObject都有一个特殊的ID，但是这个ID是一个
+        // 比较特殊的字符串，不宜用来作为图片名称的一部分。为此，我们可
+        // 以手动为这些NSManagedObject分配一个整数类型的ID
+        userDefaults.set(currentID, forKey: "PhotoID")
+        userDefaults.synchronize()
+        
+        return currentID
+    }
 }
