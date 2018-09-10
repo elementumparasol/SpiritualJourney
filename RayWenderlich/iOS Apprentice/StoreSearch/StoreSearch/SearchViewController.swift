3ef1aa5c497abe37a9499af560eed210f9f7ab12
enrica <aishiqichun@163.com>
2018-09-10 23:49:18 +0800

    布局子控件searchBar和tableView

--- RayWenderlich/iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
+++ RayWenderlich/iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    // MARK: - @IBOutlet
+    
+    /// tableView
+    @IBOutlet weak var tableView: UITableView!
+    
+    /// searchBar
+    @IBOutlet weak var searchBar: UISearchBar!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 调整tableView的边距(searchBar遮挡住了tableView第0行cell)
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
 
