358249de136b35074e9fb67749026709240543fe
enrica <aishiqichun@163.com>
2018-09-10 23:09:49 +0800

    SearchViewController

--- RayWenderlich/iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
+++ RayWenderlich/iOS Apprentice/StoreSearch/StoreSearch/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  StoreSearch
 //
 //  Created by Enrica on 2018/9/10.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
