c10f10eab02e80cf352be3c7c6f497711d2a9594
enrica <aishiqichun@163.com>
2018-09-22 16:30:36 +0800

    显示图片到imagePreview上面去

--- RayWenderlich/RxSwift Reactive Programming with Swift/C04 Observables and Subjects in Practice/Combinestagram/Combinestagram/MainViewController.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C04 Observables and Subjects in Practice/Combinestagram/Combinestagram/MainViewController.swift
@@ -7,10 +7,17 @@
 //
 
 import UIKit
+import RxSwift
 
 class MainViewController: UIViewController {
     
     // MARK: - 自定义属性
+    
+    /// disposeBag，用于管理订阅内存
+    private let disposeBag = DisposeBag()
+    
+    /// images数组(observable)
+    private let images = Variable<[UIImage]>([])
     
     
     // MARK: - @IBOutlet
@@ -27,9 +34,41 @@ class MainViewController: UIViewController {
     /// 保存按钮
     @IBOutlet weak var buttonSave: UIButton!
     
+    
+    // MARK: - @IBAction
+    
+    /// 点击导航栏右边的加号按钮调用
+    @IBAction func actionAdd(_ sender: Any) {
+        
+        images.value.append(UIImage(named: "IMG_1907")!)
+    }
+    
+    /// 点击清除按钮调用
+    @IBAction func actionClear(_ sender: Any) {
+        
+        images.value = []
+    }
+    
+    /// 点击保存按钮调用
+    @IBAction func actionSave(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 订阅images
+        images.asObservable()
+            .subscribe(onNext: { [weak self] (photos) in
+                guard let preview = self?.imagePreview else {
+                    return
+                }
+                
+                // 将拼贴完成的图片设置到imagePreview控件上面去
+                preview.image = UIImage.collage(images: photos, size: preview.frame.size)
+            })
+            .disposed(by: disposeBag)
     }
 
 
