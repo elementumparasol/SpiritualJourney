619f98a45774852e3db527f023a5d1dfea4a7b54
enrica <aishiqichun@163.com>
2018-09-25 09:55:57 +0800

    ignoreElement

--- RayWenderlich/RxSwift Reactive Programming with Swift/C05 Filtering Operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C05 Filtering Operators/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -24,3 +24,43 @@ example(of: "ignoreElements") {
     strikes.onNext("C")
     strikes.onCompleted()
 }
+
+
+
+/// elementAt
+example(of: "elementAt") {
+    
+    let strikes = PublishSubject<String>()
+    
+    let disposeBag = DisposeBag()
+    
+    strikes
+        .elementAt(2)
+        .subscribe(onNext: { (_) in
+            print("You're out!")
+        })
+        .disposed(by: disposeBag)
+    
+    strikes.onNext("A")
+    strikes.onNext("B")
+    strikes.onNext("C")
+    strikes.onNext("D")
+    strikes.onCompleted()
+}
+
+
+
+/// filter
+example(of: "filter") {
+    
+    let disposeBag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .filter({ (integer) -> Bool in  // 过滤出偶数
+            integer % 2 == 0
+        })
+        .subscribe(onNext: {  // 订阅
+            print($0)
+        })
+        .disposed(by: disposeBag)
+}
