ce5eaf64bd7a6d95666c9ac4e76412ab96e4af2b
enrica <aishiqichun@163.com>
2018-09-28 11:35:03 +0800

    搭建基本的UI界面

---
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C08 Transforming Operators in Practice/GitFeed/GitFeed/ActivityController.swift
@@ -0,0 +1,90 @@
+//
+//  ActivityController.swift
+//  GitFeed
+//
+//  Created by Enrica on 2018/9/28.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+import Kingfisher
+
+class ActivityController: UITableViewController {
+    
+    // MARK: - 自定义属性
+    
+    /// 导航栏标题
+    private let repo = "ReactiveX/RxSwift"
+    
+    /// event模型
+    private let events = Variable<[Event]>([])
+    
+    /// 用于管理资源的释放
+    private let disposeBag = DisposeBag()
+    
+    
+    // MARK: - 类自带的方法
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 设置导航栏标题
+        title = repo
+        
+        // 设置刷新控件
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1)
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "下拉刷新")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        refresh()
+    }
+
+    
+    // MARK: - 自定义方法
+    
+    /// 下拉刷新
+    @objc func refresh() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.fetchEvents(repo: strongSelf.repo)
+        }
+    }
+    
+    /// 获取数据
+    func fetchEvents(repo: String) {
+        
+        //
+    }
+
+}
+
+
+// MARK: - UITableViewDataSource
+extension ActivityController {
+    
+    // 返回Section中cell的行数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.value.count
+    }
+    
+    // 返回UITableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 取出对应的event
+        let event = events.value[indexPath.row]
+        
+        // 给cell设置数据
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = event.name
+        cell.detailTextLabel?.text = event.repo + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+        cell.imageView?.kf.setImage(with: event.imageUrl, placeholder: UIImage(named: "blank-avatar"))
+        
+        
+        return cell
+    }
+}
---
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C08 Transforming Operators in Practice/GitFeed/GitFeed/Event.swift
@@ -0,0 +1,64 @@
+//
+//  Event.swift
+//  GitFeed
+//
+//  Created by Enrica on 2018/9/28.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import Foundation
+
+/// 自定义类型
+typealias AnyDict = [String: Any]
+
+
+class Event {
+    
+    // MARK: - 模型属性
+    
+    /// repo
+    let repo: String
+    
+    /// 姓名
+    let name: String
+    
+    /// 插图
+    let imageUrl: URL
+    
+    /// 类型
+    let action: String
+    
+    
+    // MARK: - 把JSON转为Event
+    init(dict: AnyDict) {
+        
+        // 对参数dict进行校验
+        guard let repoDict = dict["repo"] as? AnyDict,
+        let actor = dict["actor"] as? AnyDict,
+        let repoName = repoDict["name"] as? String,
+        let actorName = repoDict["display_login"] as? String,
+        let actorUrlString = actor["avatar_url"] as? String,
+        let actorUrl = URL(string: actorUrlString),
+        let actionType = dict["type"] as? String else {
+            fatalError()
+        }
+        
+        // 初始化私有属性
+        repo = repoName
+        name = actorName
+        imageUrl = actorUrl
+        action = actionType
+    }
+    
+    
+    // MARK: - 把Event转为JSON
+    
+    var dict: AnyDict {
+        return [
+            "repo" : ["name": repo],
+            "actor": ["display_login": name, "avatar_url": imageUrl.absoluteString],
+            "type" : action
+        ]
+    }
+    
+}
