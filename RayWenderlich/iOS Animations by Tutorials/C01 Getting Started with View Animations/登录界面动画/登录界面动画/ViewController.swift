118cd52e63b5175e9560c903d6c9d10bf18c5aaf
enrica <aishiqichun@163.com>
2018-09-29 18:04:53 +0800

    使用Autoresizing布局子控件

--- RayWenderlich/iOS Animations by Tutorials/C01 Getting Started with View Animations/登录界面动画/登录界面动画/ViewController.swift
+++ RayWenderlich/iOS Animations by Tutorials/C01 Getting Started with View Animations/登录界面动画/登录界面动画/ViewController.swift
@@ -38,6 +38,35 @@ class ViewController: UIViewController {
     
     
     // MARK: - 类自带的方法
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 标题、用户名输入框和密码输入框在界面显示
+        // 之前，先将其移动到屏幕外面
+        titleLabel.center.x -= view.bounds.width
+        usernameTextField.center.x -= view.bounds.width
+        passwordTextField.center.x -= view.bounds.width
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 给标题文本添加动画
+        UIView.animate(withDuration: 0.5) {
+            self.titleLabel.center.x += self.view.bounds.width
+        }
+        
+        // 给用户名输入框添加动画
+        UIView.animate(withDuration: 0.5) {
+            self.usernameTextField.center.x += self.view.bounds.width
+        }
+        
+        // 给密码输入框添加动画
+        UIView.animate(withDuration: 0.5) {
+            self.passwordTextField.center.x += self.view.bounds.width
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
