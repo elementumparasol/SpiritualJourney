d9e26a6d04b333acee5961cb427cb7053422d364
enrica <aishiqichun@163.com>
2018-10-09 11:07:49 +0800

    布局UI界面

--- RayWenderlich/iOS Animations by Tutorials/C06 Introduction to Atuo Layout/自动布局/自动布局/ViewController.swift
+++ RayWenderlich/iOS Animations by Tutorials/C06 Introduction to Atuo Layout/自动布局/自动布局/ViewController.swift
@@ -8,7 +8,26 @@
 
 import UIKit
 
+/// cell可重用标识符
+private let cellIdentifier = "Cell"
+
 class ViewController: UIViewController {
+    
+    // MARK: - @IBOutlet
+    
+    /// 标题label
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    /// 添加按钮
+    @IBOutlet weak var addButton: UIButton!
+    
+    /// tableView
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - 自定义属性
+    
+    
+    // MARK: - 类自带的方法
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,5 +35,27 @@ class ViewController: UIViewController {
     }
 
 
+    // MARK: - @IBAction
+    
 }
 
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    
+    // 返回tableView中每一组cell的行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    // 返回tableView中的cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "cell --- \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
+}
