248b6dee1955a38c29689e5fde655a719ec5be8f
enrica <aishiqichun@163.com>
2018-10-21 19:45:33 +0800

    创建模型，添加图片资源

---
+++ RayWenderlich/Design Patterns by Tutorials/S02 基础设计模式/C03 MVC设计模式/RabbleWabble/RabbleWabble/Models/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Enrica on 2018/10/21.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import Foundation
+
+public struct Question {
+    
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ RayWenderlich/Design Patterns by Tutorials/S02 基础设计模式/C03 MVC设计模式/RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Enrica on 2018/10/21.
+//  Copyright © 2018 Enrica. All rights reserved.
+//
+
+import Foundation
+
+
+public struct QuestionGroup {
+    
+    public let questions: [Question]
+    public let title: String
+}
