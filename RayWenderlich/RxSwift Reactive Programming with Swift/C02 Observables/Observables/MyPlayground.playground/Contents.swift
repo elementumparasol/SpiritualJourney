c38f7ed5c0e2d1efdd6e06911d33955b27894d78
enrica <aishiqichun@163.com>
2018-11-10 17:45:25 +0800

    订阅Observable

--- RayWenderlich/RxSwift Reactive Programming with Swift/C02 Observables/Observables/MyPlayground.playground/Contents.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C02 Observables/Observables/MyPlayground.playground/Contents.swift
@@ -55,3 +55,86 @@ example(of: "from") {
     let observable = Observable.from([one, two, three])
     print(observable)
 }
+
+
+example(of: "subscribe") {
+    
+    let one = 1
+    let two = 2
+    let three = 3
+    let observable = Observable.of(one, two, three)
+    
+    // 订阅observable
+    // 针对每一个元素，observable都会抛出一个.next事件
+    // 并且，在最后还会抛出一个.completed事件作为结束
+    observable.subscribe({ event in
+        
+        // event事件里面有一个element属性，但是
+        // 只有.next事件拥有element，而.error
+        // 和.completed是没有element的，所以
+        // element是可选的，这里需要用可选绑定
+        if let element = event.element {
+            print(element)
+        }
+    })
+    
+    // 跟上面的结果一样，但是代码更简洁
+    // 因为onNext这个闭包是接收.next事件的元素作为参数的
+    // 所以我们不必手动去做可选绑定来解包
+    observable.subscribe(onNext: {element in
+        print(element)
+    })
+}
+
+
+example(of: "empty") {
+    
+    // empty()方法不接收任何参数，使用empty()方法创建
+    // 的observable是一个不包含任何东西的可观察序列
+    let observable = Observable<Void>.empty()
+    
+    // 对不包含任何东西的可观察序列进行监听，
+    // 其结果是值返回一个表示监听结束的.completed事件
+    observable.subscribe(onNext: { element in
+        
+        // 处理.next事件
+        print(element)
+    }, onError: { error in
+        
+        // 处理.error事件
+        print(error)
+    }, onCompleted: {
+        
+        // 处理.completed事件
+        print("Completed")
+    })
+}
+
+
+example(of: "never") {
+    
+    let observable = Observable<Any>.never()
+    
+    observable.subscribe(onNext: { element in
+        print(element)
+    }, onError: { error in
+        print(error)
+    }, onCompleted: {
+        print("Completed")
+    })
+}
+
+
+example(of: "range") {
+    
+    // 也可以通过range方法使用一系列值来创建一个Observable
+    let observable = Observable<Int>
+        .range(start: 1, count: 10)
+    
+    observable.subscribe(onNext: { i in
+        
+        let n = Double(i)
+        let fibonacci = Int(((pow(1.61803, n) - pow(0.61803, 2.23606)) / 2.23606).rounded())
+        print("i = \(i), fibonacci = \(fibonacci)")
+    })
+}
