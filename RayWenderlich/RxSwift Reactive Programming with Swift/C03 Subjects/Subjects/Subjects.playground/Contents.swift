06638aed063cd719d2d7306b95f6b9c5ef92446f
enrica <aishiqichun@163.com>
2018-11-13 17:33:19 +0800

    BehaviorSubject的基本使用

--- RayWenderlich/RxSwift Reactive Programming with Swift/C03 Subjects/Subjects/Subjects.playground/Contents.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C03 Subjects/Subjects/Subjects.playground/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 import RxSwift
 
-
 example(of: "PublishSubject") {
     
     // 创建一个String类型的PublishSubject实例
@@ -20,4 +19,99 @@ example(of: "PublishSubject") {
     // "1"会被打印，因为它是在订阅之后添加的事件
     subject.on(.next("1"))
     subject.onNext("This message will be listened to.")
+}
+
+
+example(of: "PublishSubject") {
+    
+    // 创建PublishSubject实例
+    let subject = PublishSubject<Int>()
+    
+    // 向subject添加事件1
+    subject.onNext(1)
+    
+    // 第一位订阅者
+    let subscriber1 = subject.subscribe(onNext: { element in
+        print("第一位订阅者: \(element)")
+    }, onCompleted: {
+        print("第一位订阅者: Completed")
+    })
+    
+    // 向subject添加事件2
+    subject.onNext(2)
+    
+    // 第二位订阅者
+    let subscriber2 = subject.subscribe(onNext: { element in
+        print("第二位订阅者: \(element)")
+    }, onCompleted: {
+        print("第二位订阅者: Completed")
+    })
+    
+    // 向subject添加事件3
+    subject.onNext(3)
+    
+    // 调用dispose()，表示第一位订阅者已经完成了
+    // 所以，它无法再接收到后面添加的事件了
+    subscriber1.dispose()
+
+    // 第二位订阅者还未完成，所以它可以接收事件4
+    subject.onNext(4)
+    
+    // 向subject添加.completed事件，则表示subject
+    // 已经顺利完成任务，然后停止了。subject只会向当前
+    // 和未来的订阅者发送.completed事件，其它任何事件
+    // 均无法发送
+    subject.onCompleted()
+    
+    // 由于第一位订阅者已经完成了，所以它不能接收任何事件，
+    // 包括.completed事件。但是，第二位订阅者还没有完成，
+    // 所以它可以接收subject发出的.completed事件，但是
+    // 无法接收后面新添加的事件5
+    subject.onNext(5)
+    subscriber2.dispose()
+    
+    let disposeBag = DisposeBag()
+
+    // 再创建一个新的订阅者
+    subject.subscribe {
+        print("A new subscription:", $0.element ?? $0)
+    }.disposed(by: disposeBag)
+
+    // 新的订阅者无法再接收到事件6了。因为在代码第65行
+    // 已经添加了.completed事件。所以，即便我们后来
+    // 又创建了新的订阅者，它也只能收到之前发出的completed事件
+    subject.onNext(6)
+}
+
+
+/// 自定义额错误类型
+enum MyError: Error {
+    case anError
+}
+
+/// 自定义一个print函数
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+    
+    // 如果element存在，则打印element；
+    // 如果element不存在，但是error存在，则打印error；
+    // 如果element和error都不存在，则打印event
+    print(label, (event.element ?? event.error) ?? event)
+}
+
+example(of: "BehaviorSubject") {
+    
+    let subject = BehaviorSubject(value: "Initial value")
+    let disposeBag = DisposeBag()
+    
+    subject.onNext("X")
+    
+    subject.subscribe({
+        print(label: "1)", event: $0)
+    }).disposed(by: disposeBag)
+    
+    subject.onError(MyError.anError)
+    
+    subject.subscribe({
+        print(label: "2)", event: $0)
+    }).disposed(by: disposeBag)
 }
