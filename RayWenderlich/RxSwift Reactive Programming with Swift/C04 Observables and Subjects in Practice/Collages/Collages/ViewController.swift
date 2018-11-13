497455af4375acbd2ae3cbeb841a303814028013
enrica <aishiqichun@163.com>
2018-11-13 23:08:07 +0800

    布局子控件

--- RayWenderlich/RxSwift Reactive Programming with Swift/C04 Observables and Subjects in Practice/Collages/Collages/ViewController.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C04 Observables and Subjects in Practice/Collages/Collages/ViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet
+    
+    /// imageView
+    @IBOutlet weak var imageView: UIImageView! {
+        
+        didSet {
+            imageView.layer.masksToBounds = true
+            imageView.layer.cornerRadius = 10.0
+            imageView.layer.borderWidth = 1.0
+            imageView.layer.borderColor = UIColor.black.cgColor
+        }
+    }
+    
+    /// clearButton
+    @IBOutlet weak var clearButton: UIButton! {
+        
+        didSet {
+            clearButton.layer.masksToBounds = true
+            clearButton.layer.cornerRadius = 10.0
+        }
+    }
+    
+    /// saveButton
+    @IBOutlet weak var saveButton: UIButton! {
+        
+        didSet {
+            saveButton.layer.masksToBounds = true
+            saveButton.layer.cornerRadius = 10.0
+        }
+    }
+    
+    /// addButton
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
