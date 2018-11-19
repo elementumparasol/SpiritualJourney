ec4a5617b8b30dda09ed09266502144ddb4c0c37
enrica <aishiqichun@163.com>
2018-11-19 14:43:43 +0800

    使用filter函数筛选出满足特定条件的元素

--- RayWenderlich/RxSwift Reactive Programming with Swift/C05 Filtering Operators/Filtering Operators/Filtering Operators.playground/Contents.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C05 Filtering Operators/Filtering Operators/Filtering Operators.playground/Contents.swift
@@ -6,6 +6,11 @@ example(of: "igonreElements") {
     let strikes = PublishSubject<String>()
     let disposeBag = DisposeBag()
     
+    // ignoreElements会忽略.next事件，但是
+    // 对于像stop事件(.error或者.completed)
+    // 则不会过滤。如果你希望一个Observable在
+    // 结束(正常或者非正常结束)之后接收到相应的
+    // 通知，那么ignoreElements就能排上用场
     strikes.ignoreElements().subscribe { _ in
         print("You're out!")
     }.disposed(by: disposeBag)
@@ -14,6 +19,7 @@ example(of: "igonreElements") {
     strikes.onNext("B")
     strikes.onNext("C")
     
+    // .completed事件不会被过滤
     strikes.onCompleted()
 }
 
@@ -23,12 +29,34 @@ example(of: "elementAt") {
     let strikes = PublishSubject<String>()
     let disposeBag = DisposeBag()
     
+    // elementAt主要是用来接收第n个事件元素，而对于
+    // 其它的，则全部忽略。比如说，我们只接收下标值为2
+    // 的事件元素，其它的则全部忽略
     strikes.elementAt(2).subscribe(onNext: { (_) in
         print("You're out!")
     }).disposed(by: disposeBag)
     
     strikes.onNext("A")
     strikes.onNext("B")
+    
+    // 只接收下标值为2的事件元素
     strikes.onNext("C")
-    strikes.onNext("D")
+}
+
+
+example(of: "filter") {
+    
+    let disposeBag = DisposeBag()
+    
+    // 创建一个Observable，并且使用filter对其进行过滤
+    Observable.of(1, 2, 3)
+        .filter({ (integer) -> Bool in
+            
+            // 过滤出所有小于3的数
+            integer < 3
+        }).subscribe(onNext: {
+            
+            // 打印过滤出来的元素
+            print($0)
+        }).disposed(by: disposeBag)
 }
