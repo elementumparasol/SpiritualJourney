9dc58ffc87646dade83ea22cfe117ec6ded35ab1
enrica <aishiqichun@163.com>
2018-11-21 04:03:10 +0800

    distinct operators

--- RayWenderlich/RxSwift Reactive Programming with Swift/C05 Filtering Operators/Filtering Operators/Filtering Operators.playground/Contents.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C05 Filtering Operators/Filtering Operators/Filtering Operators.playground/Contents.swift
@@ -188,3 +188,55 @@ example(of: "takeUntil") {
     subject.onNext("C")
     subject.onNext("D")
 }
+
+
+example(of: "distinctUntilChanged") {
+    
+    let disposeBag = DisposeBag()
+    
+    // 连续且重复的事件元素会被过滤掉
+    Observable.of(1, 2, 2, 1, 5)
+        .distinctUntilChanged()
+        .subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+}
+
+
+example(of: "distinctUntilChanged(_ :)") {
+
+    let disposeBag = DisposeBag()
+
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+
+    Observable<NSNumber>.of(10, 110, 20, 200, 210, 310)
+        .distinctUntilChanged({ (a, b) -> Bool in
+
+            guard let aWords = formatter
+                .string(from: a)?
+                .components(separatedBy: " "),
+                let bWords = formatter
+                    .string(from: b)?
+                    .components(separatedBy: " ")
+                else { return false }
+
+            var containsMatch = false
+
+            for aWord in aWords {
+                for bWord in bWords {
+                    if aWord == bWord {
+                        containsMatch = true
+                        break
+                    }
+                }
+            }
+
+            return containsMatch
+        }).subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+}
+
+
+
