413563a11c882400b24bb39003615380bf0dfb01
enrica <aishiqichun@163.com>
2018-11-28 21:31:55 +0800

    从服务器获取response

--- RayWenderlich/RxSwift Reactive Programming with Swift/C08 Transforming Operators in Practice/Github/Github/ViewController.swift
+++ RayWenderlich/RxSwift Reactive Programming with Swift/C08 Transforming Operators in Practice/Github/Github/ViewController.swift
@@ -7,24 +7,71 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import Kingfisher
 
 class ViewController: UITableViewController {
     
     // MARK: - 自定义属性
     
-    /// 导航栏标题
-    private let titleString = "ReactiveX/RxSwift"
+    /// 导航栏标题(也是GitHub仓库名称)
+    private let repo = "ReactiveX/RxSwift"
     
-    ///
+    /// 数据模型
+    private let events = Variable<[Event]>([])
+    
+    /// 用于内存管理
+    private let disposeBag = DisposeBag()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 设置导航栏标题
-        title = titleString
+        title = repo
         
         
     }
+    
+    
+    // MARK: - 自定义方法
+    
+    /// 获取网络数据
+    ///
+    /// - Parameter repo: GitHub仓库名称
+    func fetchEvents(repo: String) {
+        
+        // 创建Observable实例
+        let response = Observable
+            .from([repo])  // from方法接收一个数组参数来创建Observable实例
+            .map { (repo) -> URL in  // 拼接URL字符串
+                return URL(string: "https://api.github.com/repos/\(repo)/events")!
+            }
+            .map { (url) -> URLRequest in  // 将URL地址转换为URLRequest
+                return URLRequest(url: url)
+            }
+            // 使用RxCocoa的response(request: )方法。当该方法完成时
+            // 我们会从服务器上收到一个完整的相应，并且，该方法会返回一个
+            // Observable<(response: HTTPURLResponse, data: Data)>
+            .flatMap { (request) -> Observable<(response: HTTPURLResponse, data: Data)> in
+                
+                // 发送网络请求
+                return URLSession.shared.rx.response(request: request)
+        }.share(replay: 1, scope: .whileConnected)
+        
+        // 过滤出所有成功的代码(也就是范围在200和300之间的状态码)，
+        // 然后让其通过
+        
+        // ~=运算符与左侧的范围一起使用时，用于检测该范围是否
+        // 包含该运算符右侧的值
+        
+        
+        
+    }
+    
+    
+    
 
     // MARK: UITableViewDataSource
     
