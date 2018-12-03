a1bc0329b19c7fa31988eb547ceb06bc138b7f28
enrica <aishiqichun@163.com>
2018-12-03 15:05:21 +0800

    存储数据类型

--- RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S02 Creating a Realm Data Model/C03 Object Basics and Data types/RealmPlayground/RealmPlayground.playground/Contents.swift
+++ RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S02 Creating a Realm Data Model/C03 Object Basics and Data types/RealmPlayground/RealmPlayground.playground/Contents.swift
@@ -40,3 +40,76 @@ Example.of("Basic Model") {
     let car = Car(brand: "BMW", year: 1980)
     print(car)
 }
+
+
+
+class Person: Object {
+    
+    // 对象类型 - String Type
+    @objc dynamic var firstName = ""
+    @objc dynamic var lastName: String?
+    
+    // 对象类型 - Date Type
+    @objc dynamic var born = Date.distantPast
+    @objc dynamic var deceased: Date?
+    
+    // 对象类型 - Data Type
+    @objc dynamic var photo: Data?
+    
+    
+    // 原始类型 - Bool
+    @objc dynamic var isVIP: Bool = false
+    let allowsPublication = RealmOptional<Bool>()
+    
+    // 原始类型 - Int
+    @objc dynamic var id = 0
+    @objc dynamic var hairCount: Int64 = 0
+    
+    // 原始类型 - Float
+    @objc dynamic var height: Float = 0.0
+    @objc dynamic var weight = 0.0  // Double类型
+    
+    
+    // 自定义类型 - 使用计算属性来自定义类型
+    private let lat = RealmOptional<Double>()
+    private let lng = RealmOptional<Double>()
+    var lastLocation: CLLocation? {
+        
+        get {
+            guard let lat = lat.value,
+                let lng = lng.value else {
+                    return nil
+            }
+            
+            return CLLocation(latitude: lat, longitude: lng)
+        }
+        
+        set {
+            
+            guard let location = newValue?.coordinate else {
+                lat.value = nil
+                lng.value = nil
+                return
+            }
+            
+            lat.value = location.latitude
+            lng.value = location.longitude
+        }
+    }
+    
+    // 自定义类型 - 枚举
+    enum Department: String {
+        case technology
+        case politics
+        case business
+        case health
+        case science
+        case sports
+        case travel
+    }
+    @objc dynamic private var _department = Department.technology.rawValue
+    var department: Department {
+        get { return Department(rawValue: _department)! }
+        set { _department = newValue.rawValue }
+    }
+}
