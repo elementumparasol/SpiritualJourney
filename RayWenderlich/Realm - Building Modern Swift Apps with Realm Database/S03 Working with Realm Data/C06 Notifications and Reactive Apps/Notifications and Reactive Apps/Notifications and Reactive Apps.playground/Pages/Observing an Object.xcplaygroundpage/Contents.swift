ebd46fcb3b49f897711f884168517605114b2227
enrica <aishiqichun@163.com>
2018-12-08 18:00:39 +0800

    Observing an object

--- RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S03 Working with Realm Data/C06 Notifications and Reactive Apps/Notifications and Reactive Apps/Notifications and Reactive Apps.playground/Pages/Observing an Object.xcplaygroundpage/Contents.swift
+++ RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S03 Working with Realm Data/C06 Notifications and Reactive Apps/Notifications and Reactive Apps/Notifications and Reactive Apps.playground/Pages/Observing an Object.xcplaygroundpage/Contents.swift
@@ -6,6 +6,52 @@ Example.of("Observing an Object")
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 
-
+// 设置Realm的配置选项(在内存中安装Realm)
 let configuration = Realm.Configuration(inMemoryIdentifier: "TemporaryRealm")
+
+// 根据配置选项来安装Realm
 let realm = try! Realm(configuration: configuration)
+
+// 创建一个Article实例，并且设置它的id
+let article = Article()
+article.id = "new-article"
+
+// 将实例article添加到Realm中
+try! realm.write {
+    realm.add(article)
+}
+
+// 监听article的改变
+// observe(_ block: @escaping (ObjectChange) -> Void) -> NotificationToken
+let token = article.observe { (change) in
+    
+    switch change {
+    case .change(let properties):
+        for property in properties {
+            switch property.name {
+            case "title":
+                print("📝 Article title changed from \(property.oldValue ?? "nil") to \(property.newValue ?? "nil")")
+            case "author":
+                print("👤 Author changed to \(property.newValue ?? "nil")")
+            default:
+                break
+            }
+        }
+        
+        if properties.contains(where: { $0.name == "date" }) {
+            print("Date has changed to \(article.date)")
+        }
+    case .error(let error):
+        print("Error occurred: \(error)")
+    case .deleted:
+        print("Article was deleted.")
+    }
+}
+
+print("Subscription token: \(token)")
+
+
+// 修改article实例的某个属性
+try! realm.write {
+    article.title = "Work in progress"
+}
