a85c3ecfeb35301d97e54f28c409bb9caedbc8f8
enrica <aishiqichun@163.com>
2018-12-10 11:42:51 +0800

    Observing an entire Realm

--- RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S03 Working with Realm Data/C06 Notifications and Reactive Apps/Notifications and Reactive Apps/Notifications and Reactive Apps.playground/Pages/Realm-wide notifications.xcplaygroundpage/Contents.swift
+++ RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S03 Working with Realm Data/C06 Notifications and Reactive Apps/Notifications and Reactive Apps/Notifications and Reactive Apps.playground/Pages/Realm-wide notifications.xcplaygroundpage/Contents.swift
@@ -5,6 +5,16 @@ Example.of("Realm wide notifications")
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 
-
+// 设置Realm安装配置信息
 let configuration = Realm.Configuration(inMemoryIdentifier: "TemporaryRealm")
+
+// 在内存中创建Realm数据库
 let realm = try! Realm(configuration: configuration)
+
+// 监听整个Realm数据库的通知
+let token = realm.observe { (notification, realm) in
+    print(notification)
+}
+
+// 提交一个空的write处理
+try! realm.write { }
