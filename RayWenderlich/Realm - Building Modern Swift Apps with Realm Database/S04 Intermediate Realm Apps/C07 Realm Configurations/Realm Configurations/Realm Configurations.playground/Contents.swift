f4eb05cdf87f556baf3352b070e0be5804b88c7e
enrica <aishiqichun@163.com>
2018-12-13 15:17:43 +0800

    Default configuration operations

--- RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S04 Intermediate Realm Apps/C07 Realm Configurations/Realm Configurations/Realm Configurations.playground/Contents.swift
+++ RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S04 Intermediate Realm Apps/C07 Realm Configurations/Realm Configurations/Realm Configurations.playground/Contents.swift
@@ -10,3 +10,30 @@ Example.of("New configuration") {
     // 打印默认的配置信息
     print(configuration)
 }
+
+
+Example.of("Default configuration") {
+    
+    // 获取默认的配置
+    let defaultConfiguration = Realm.Configuration
+        .defaultConfiguration
+    
+    // 打印默认的配置信息
+    print(defaultConfiguration)
+}
+
+
+Example.of("Custom Configuration") {
+    
+    var defaultConfig = Realm.Configuration
+        .defaultConfiguration
+    
+    print(defaultConfig.schemaVersion)
+    
+    // 更改schemaVersion(UInt64)
+    defaultConfig.schemaVersion = 1
+    
+    Realm.Configuration.defaultConfiguration = defaultConfig
+    
+    print(defaultConfig.schemaVersion)
+}
