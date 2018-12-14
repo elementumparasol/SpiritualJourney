ac380f875ec8278a7df5d1310f4177c45d5a8401
enrica <aishiqichun@163.com>
2018-12-14 23:13:28 +0800

    Documents folder

--- RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S04 Intermediate Realm Apps/C07 Realm Configurations/Realm Configurations/Realm Configurations.playground/Contents.swift
+++ RayWenderlich/Realm - Building Modern Swift Apps with Realm Database/S04 Intermediate Realm Apps/C07 Realm Configurations/Realm Configurations/Realm Configurations.playground/Contents.swift
@@ -37,3 +37,37 @@ Example.of("Custom Configuration") {
     
     print(defaultConfig.schemaVersion)
 }
+
+
+Example.of("In-Memory Configuration") {
+    
+    let memoryConfig1 = Realm.Configuration(inMemoryIdentifier: "InMemoryIdentifier1")
+    // print(memoryConfig1)
+    
+    let memoryConfig2 = Realm.Configuration(inMemoryIdentifier: "InMemoryIdentifier2")
+    // print(memoryConfig2)
+    
+    let realm1 = try! Realm(configuration: memoryConfig1)
+    let people1 = realm1.objects(Person.self)
+    try! realm1.write {
+        realm1.add(Person())
+    }
+    print("People (1): \(people1.count)")
+
+    let realm2 = try! Realm(configuration: memoryConfig2)
+    let people2 = realm2.objects(Person.self)
+    print("People (2): \(people2.count)")
+}
+
+
+Example.of("Documents folder") {
+    
+    // 在Documents目录下面自定义一个文件路径
+    let documentsURL = try! FileManager.default
+        .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+        .appendingPathComponent("myRealm.realm")
+    
+    // 在沙盒中的Documents文件夹下面创建Realm文件
+    let documentsConfig = Realm.Configuration(fileURL: documentsURL)
+    print("Documents-folder Realm in: \(documentsConfig)")
+}
